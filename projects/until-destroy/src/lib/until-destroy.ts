b8e148a9138874796294fd4c0f96c080ae60f552
Marcell Kiss <kissmarcell88@gmail.com>
2022-06-30 08:39:51 +0200

    feat: add proper docs

--- projects/until-destroy/src/lib/until-destroy.ts
+++ projects/until-destroy/src/lib/until-destroy.ts
@@ -1,5 +1,12 @@
-import { takeUntil } from 'rxjs';
+import { MonoTypeOperatorFunction, takeUntil } from 'rxjs';
 
-export function untilDestroy$(component: any) {
-  return takeUntil(component.destroy$);
+export function untilDestroy$<T>(
+  componentInstance: any
+): MonoTypeOperatorFunction<T> {
+  if (!componentInstance.destroy$) {
+    throw new Error(
+      `Decorate your component with @WithDestroy$() to be able to use untilDestroy$`
+    );
+  }
+  return takeUntil(componentInstance.destroy$);
 }
